b4c5f4c8c2a709e95717244c2613ef51ddb2689a
Rian Santos <rian.sn31@gmail.com>
2026-03-23 15:29:43 -0300

    fix: Add buttons to change canvas colors, draw a square, and say hello

--- js/script.js
+++ js/script.js
@@ -13,3 +13,31 @@ function changeColor() {
     element2.classList.add("fuchsiaback");
   }
 }
+let isSquareDrawn = false;
+let isHelloSaid = false;
+
+function doSquare() {
+  var canvas = document.getElementById("canva1");
+  var ctx = canvas.getContext("2d");
+  if (!isSquareDrawn) {
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(50, 30, 100, 100);
+    isSquareDrawn = true;
+  } else {
+    ctx.clearRect(50, 30, 100, 100);
+    isSquareDrawn = false;
+  }
+}
+function sayHello() {
+  var canvas = document.getElementById("canva2");
+  var ctx = canvas.getContext("2d");
+  if (isHelloSaid) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    isHelloSaid = false;
+    return;
+  } else {
+    ctx.font = "30px Arial";
+    ctx.fillText("Hello World", 10, 50);
+    isHelloSaid = true;
+  }
+}
